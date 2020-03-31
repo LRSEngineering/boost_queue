83b81a4fc90f34846e8018807c117623928ac371
Faisal Ali <dev.faisalali@gmail.com>
2020-03-31 16:29:46 -0500

    Missed incrementing patch version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ mod = Extension(
 
 setup(
     name='boost_queue',
-    version='0.5.4',
+    version='0.5.5',
     description="Queue using boost's locking API",
     long_description=open('README.rst').read(),
     classifiers=[
